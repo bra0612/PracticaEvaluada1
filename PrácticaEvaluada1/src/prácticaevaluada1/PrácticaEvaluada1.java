98efd8bba535e428444a81cf3038bbc3ad5114de
Brayan Molina Pizarro <molinapizarrobrayan@gmail.com>
2025-10-01 20:51:38 -0600

    Add files via upload

---
+++ PrácticaEvaluada1/src/prácticaevaluada1/PrácticaEvaluada1.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prácticaevaluada1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Brayan Molina
+ */
+public class PrácticaEvaluada1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        //Input de datos:
+        String nombre = "";
+        nombre = JOptionPane.showInputDialog("Digite su nombre:");
+
+        String apellido = "";
+        apellido = JOptionPane.showInputDialog("Digite su apellido:");
+
+        String lectura;
+        double salario;
+        double impuestoSalario = salario * 100 / 0.10;
+        lectura = JOptionPane.showInputDialog("Digite su salario en colones:");
+        salario = Double.parseDouble(lectura);
+
+        //IF e IF ELSE:
+        if (salario >= 1352000) {
+            impuestoSalario + salario;
+            System.out.println("");
+
+        }
+
+    }
+
+}
